18bf1cb624b04bd0bcd172d7ef335be023249443
Ming <ktaemin13@naver.com>
2023-07-07 18:28:15 +0900

    first commit

---
+++ src/main/java/cozycorner/CozycornerApplication.java
@@ -0,0 +1,13 @@
+package cozycorner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CozycornerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CozycornerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cozycorner/cozycorner/CozycornerApplicationTests.java
@@ -0,0 +1,13 @@
+package cozycorner.cozycorner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CozycornerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
