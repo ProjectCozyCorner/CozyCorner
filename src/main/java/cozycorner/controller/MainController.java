669a44c5da6166cc008cdaa3c553cb42265213d7
ming <mingssssss@github.com>
2023-07-08 14:41:35 +0900

    test

--- src/main/java/cozycorner/controller/MainController.java
+++ src/main/java/cozycorner/controller/MainController.java
@@ -1,14 +1,37 @@
 package cozycorner.controller;
 
+import cozycorner.model.User;
+import cozycorner.repo.UserRepo;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class MainController {
 
+    public MainController(UserRepo userRepo) {
+        this.userRepo = userRepo;
+    }
+
     @RequestMapping("/")
     public String welcomePage() {
 
-        return "index";
+        return "index_test";
+    }
+
+    private UserRepo userRepo;
+
+    @GetMapping("/welcome")
+    public String welcomePage(Model model) {
+        User user = new User();
+        user.setUid("Hello");
+        user.setPw("World");
+        userRepo.save(user);
+        List<User> userList = userRepo.findAll();
+        model.addAttribute(userList);
+        return "welcome";
     }
 }
