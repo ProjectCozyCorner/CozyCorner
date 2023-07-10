eabb0d1978835664b10bf4a5687989d55b25570b
yhn032 <87313203+yhn032@users.noreply.github.com>
2023-07-10 17:12:00 +0900

    각 페이지별 링크 연결 임시로 MainController에 request mapping 해두었는데 프론트 단도 기능별로 패키지 구별해야 할 것 같음 20230710 Leo

--- src/main/java/cozycorner/controller/MainController.java
+++ src/main/java/cozycorner/controller/MainController.java
@@ -10,4 +10,35 @@ import java.util.List;
 @Controller
 public class MainController {
 
+    @GetMapping("/home")
+    public String home(){
+        return "index";
+    }
+
+    @GetMapping("/shop")
+    public String shop(){
+        return "shop";
+    }
+
+    @GetMapping("/cart")
+    public String cart(){
+        return "cart";
+    }
+
+    @GetMapping("/checkOut")
+    public String checkOut(){
+        return "checkout";
+    }
+
+    @GetMapping("/shopDetail")
+    public String shopDetail(){
+        return "detail";
+    }
+
+    @GetMapping("/contact")
+    public String contact(){
+        return "contact";
+    }
+
+
 }
